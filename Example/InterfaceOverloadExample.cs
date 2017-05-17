6d1da68ac32ea3a5a293d780b2f0463a7f8bdba1
Fyzxs <Fyzxs@users.noreply.github.com>
2017-05-17 14:43:33 -0700

    CSharp Interface Overload impl

---
+++ Example/InterfaceOverloadExample.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Example
+{
+    [TestClass]
+    public class InterfaceOverloadExample
+    {
+        [TestMethod]
+        public void BaseClassShouldImplementTheMethod()
+        {
+            new BaseClass().TheMethod(1, false);
+        } 
+
+        [TestMethod]
+        public void DerivedClassShouldBeABaseClass()
+        {
+            BaseClass example = new DerivedClass();
+        }
+
+        [TestMethod]
+        public void DerivedClassShouldBeAnTheInterface()
+        {
+            ITheInterface example = new DerivedClass();
+        }
+
+        [TestMethod]
+        public void TheInterfaceShouldImeplementTheMethod()
+        {
+            ITheInterface example = new DerivedClass();
+
+            string stringVal = example.TheMethod(1, true);
+        }
+
+        [TestMethod]
+        public void BaseClassImplUsedThroughTheInterface()
+        {
+            ITheInterface example = new DerivedClass();
+
+            string stringVal = example.TheMethod(1, true);
+
+            Assert.AreEqual("StringVal", stringVal);
+        }
+
+        public interface ITheInterface {
+            string TheMethod(int intVal, bool boolVal);
+        }
+
+        public class DerivedClass : BaseClass, ITheInterface { }
+
+        public class BaseClass {
+            public string TheMethod(int intVal, bool boolVal) => "StringVal";
+        }
+    }
+}
---
+++ Example/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Example")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Example")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("92041e1c-168e-475e-b8c4-25209c1c4cbd")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
