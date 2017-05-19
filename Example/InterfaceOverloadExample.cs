58a9b774c8025db344a40eacf03376c9cc5b2949
Fyzxs <Fyzxs@users.noreply.github.com>
2017-05-18 18:52:28 -0700

    CSharp Interface Overload impl for UI example

--- Example/InterfaceOverloadExample.cs
+++ Example/InterfaceOverloadExample.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Configuration;
+using System.Net.Mime;
+using System.Runtime.InteropServices;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Example
@@ -6,22 +9,30 @@ namespace Example
     [TestClass]
     public class InterfaceOverloadExample
     {
+
+        #region Simple Example Of Interface Overload
         [TestMethod]
         public void BaseClassShouldImplementTheMethod()
         {
-            new BaseClass().TheMethod(1, false);
+            string stringVal = new BaseClass().TheMethod(1, false);
+
+            Assert.AreEqual("StringVal", stringVal);
         } 
 
         [TestMethod]
         public void DerivedClassShouldBeABaseClass()
         {
             BaseClass example = new DerivedClass();
+
+            Assert.IsNotNull(example);
         }
 
         [TestMethod]
         public void DerivedClassShouldBeAnTheInterface()
         {
             ITheInterface example = new DerivedClass();
+
+            Assert.IsNotNull(example);
         }
 
         [TestMethod]
@@ -30,6 +41,8 @@ namespace Example
             ITheInterface example = new DerivedClass();
 
             string stringVal = example.TheMethod(1, true);
+
+            Assert.AreEqual("StringVal", stringVal);
         }
 
         [TestMethod]
@@ -51,5 +64,78 @@ namespace Example
         public class BaseClass {
             public string TheMethod(int intVal, bool boolVal) => "StringVal";
         }
+
+        #endregion
+
+        #region UI-ish Example of Interface Overload
+
+        [TestMethod]
+        public void UiWidgetHasTextSetter()
+        {
+            BaseUiWidget widget = new BaseUiWidget {Text = "Setting String"};
+
+
+            Assert.AreEqual("Setting String", widget.Text);
+        }
+
+        [TestMethod]
+        public void UiWidgetHasTextGetter()
+        {
+            BaseUiWidget widget = new BaseUiWidget { Text = "Getting String" };
+
+            string text = widget.Text;
+
+            Assert.AreEqual("Getting String", text);
+        }
+
+        [TestMethod]
+        public void WrappedUiWidgetShouldExtendSetTexte()
+        {
+            ISetText enc = new WrappedUiWidget();
+
+            Assert.IsNotNull(enc);
+        }
+
+        [TestMethod]
+        public void WrappedUiWidgetShouldExtenBaseUiWidget()
+        {
+            BaseUiWidget enc = new WrappedUiWidget();
+
+            Assert.IsNotNull(enc);
+        }
+
+        [TestMethod]
+        public void EncapsulatedDataShouldWriteToSetText()
+        {
+            EncapsulatedData data = new EncapsulatedData("It's Non-Mutable");
+            WrappedUiWidget widget = new WrappedUiWidget();
+
+            data.DisplayValue(widget);
+
+            Assert.AreEqual("It's Non-Mutable", widget.Text);
+        }
+
+        public class WrappedUiWidget : BaseUiWidget, ISetText{}
+
+        public class BaseUiWidget {//Like a TextBox
+            public string Text { get;  set; }
+        }
+
+        public interface ISetText { string Text { set; } }
+
+        public partial class EncapsulatedData
+        {
+            private readonly string _value;
+
+            public EncapsulatedData(string value) => _value = value;
+        }
+
+        //This Partial has the methods to interact with UI Elements
+        public partial class EncapsulatedData
+        {
+            public void DisplayValue(ISetText widget) => widget.Text = _value;
+        }
+
+        #endregion
     }
 }
